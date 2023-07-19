ee3207caba5591999fd1c8232fde11ca7c3cbfb8
Vinicius Santos <viniciusantos0898@gmail.com>
2023-07-19 15:59:53 -0300

    Changed the splash screen

--- app/src/main/java/com/example/taskcontrol/uxui/SplashScreen.kt
+++ app/src/main/java/com/example/taskcontrol/uxui/SplashScreen.kt
@@ -1,36 +1,39 @@
 package com.example.taskcontrol.uxui
 
-import android.graphics.drawable.Drawable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskcontrol.R
+import com.example.taskcontrol.ui.theme.TaskControlTheme
 
 @Preview
 @Composable
 fun ShowSplashFragment(){
+    TaskControlTheme(darkTheme = false) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.primary),
+            contentAlignment = Alignment.Center
+        ){
 
-    Box(
-        Modifier.fillMaxSize().background(MaterialTheme.colorScheme.primary),
-      contentAlignment = Alignment.Center
-    ){
-        Image(
-            painter = painterResource(id = R.drawable.ic_logo),
-            contentDescription = "SplashLogo",
-            modifier = Modifier.size(250.dp),
-        )
+            Text(
+                stringResource(id = R.string.splash_screen_msg),
+                color = MaterialTheme.colorScheme.background,
+                fontSize = 25.sp
+            )
+        }
     }
-
 }
